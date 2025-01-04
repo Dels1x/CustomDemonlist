b177019a613354eb5f1b4d25447291dfd19ccc36
Dels1x <delsixreborn@gmail.com>
2025-01-05 00:38:06 +0200

    feat: Google OAuth2 button for authentication

---
+++ src/main/custom-demonlist-frontend/api/api.ts
@@ -0,0 +1,1 @@
+const API_URL: string = "http://localhost:8080/";
--- src/main/custom-demonlist-frontend/pages/index.tsx
+++ src/main/custom-demonlist-frontend/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import GoogleButton from "react-google-button";
 
 export default function Home() {
   return (
@@ -11,7 +12,13 @@ export default function Home() {
       </Head>
 
         <main>
-            wassup
+            <a  target="_blank"
+                href="https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&client_id=282635553570-0pf2544q5ksv6ajqns8qb8md775cfqn8.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Foauth2%2Fcallback%2Fgoogle&scope=openid%20email%20profile&state=random_state_string&access_type=offline&prompt=consent&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow">
+                <GoogleButton
+                    label='Sign in with Google'
+                    onClick={() => { console.log('Google button clicked') }}
+                />
+            </a>
         </main>
     </>
   );
