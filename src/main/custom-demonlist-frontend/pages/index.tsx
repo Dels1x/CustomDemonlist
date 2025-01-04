ebab55d0b63b26020cc2479cb1403e4985a4c153
Dels1x <delsixreborn@gmail.com>
2025-01-04 22:11:43 +0200

    init: start frontend Next.js TypeScript development

---
+++ src/main/custom-demonlist-frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Custom Demonlist" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+        <main>
+            wassup
+        </main>
+    </>
+  );
+}
