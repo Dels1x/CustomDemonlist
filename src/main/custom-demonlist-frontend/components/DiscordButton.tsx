698614cc59dea39fcc5e8aa9c9f68a14bed0d5af
Dels1x <delsixreborn@gmail.com>
2025-01-09 21:17:03 +0200

    style: improve styles for Discord OAuth2 button (and add Discord logo image inside it)

--- src/main/custom-demonlist-frontend/components/DiscordButton.tsx
+++ src/main/custom-demonlist-frontend/components/DiscordButton.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import styles from "@/styles/DiscordButton.module.css";
+import Image from "next/image";
 
 interface DiscordButtonProps {
     label?: string;
 }
 
-const DiscordButton: React.FC<DiscordButtonProps> = ({ label = "Sign in with Discord" }) => {
+const DiscordButton: React.FC<DiscordButtonProps> = ({label = "Sign in with Discord"}) => {
     const handleClick = () => {
         window.location.href = "https://discord.com/oauth2/authorize?client_id=1310700970316922990&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Foauth2%2Fcallback%2Fdiscord&scope=identify+email"
     }
     return (
-        <button
-            onClick={handleClick}
-            className={styles.button}
-        >
-            {label}
-        </button>
+        <div className={styles.button}>
+            <Image
+                src={"discord_logo.svg"}
+                alt="Discord"
+                width={20}
+                height={20}
+                unoptimized={true}/>
+            <button onClick={handleClick}>
+                {label}
+            </button>
+        </div>
     );
 };
 
--- src/main/custom-demonlist-frontend/pages/index.tsx
+++ src/main/custom-demonlist-frontend/pages/index.tsx
@@ -3,8 +3,6 @@ import GoogleButton from "react-google-button";
 import React from "react";
 import DiscordButton from "@/components/DiscordButton";
 
-require('dotenv').config();
-
 export default function Home() {
     const handleGoogleSignIn = () => {
         window.location.href = "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&client_id=282635553570-0pf2544q5ksv6ajqns8qb8md775cfqn8.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Foauth2%2Fcallback%2Fgoogle&scope=openid%20email%20profile&state=random_state_string&access_type=offline&prompt=consent&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow";
