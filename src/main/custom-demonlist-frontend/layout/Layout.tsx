4d1120d267979050a1cd4df23663388261dc7d25
Dels1x <delsixreborn@gmail.com>
2025-01-12 23:04:51 +0200

    feat: added Layout (MVP)

---
+++ src/main/custom-demonlist-frontend/layout/Layout.tsx
@@ -0,0 +1,32 @@
+import React, {ReactNode} from "react";
+import styles from "@/styles/Layout.module.css";
+import Head from "next/head";
+import Link from "next/link";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
+    console.log(children);
+
+    return (
+        <div>
+            <Head>
+                <title>Page example</title>
+                <meta name="description" content="Custom Demonlist"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
+            <nav className={styles.nav}>
+                <Link href="/account">
+                    Sign up
+                </Link>
+            </nav>
+            <main>
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
--- src/main/custom-demonlist-frontend/pages/index.tsx
+++ src/main/custom-demonlist-frontend/pages/index.tsx
@@ -1,27 +1,13 @@
-import Head from "next/head";
 import GoogleButton from "react-google-button";
 import React from "react";
 import DiscordButton from "@/components/DiscordButton";
+import Layout from "@/layout/Layout";
 
 export default function Home() {
-    const handleGoogleSignIn = () => {
-        window.location.href = "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&client_id=282635553570-0pf2544q5ksv6ajqns8qb8md775cfqn8.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Foauth2%2Fcallback%2Fgoogle&scope=openid%20email%20profile&state=random_state_string&access_type=offline&prompt=consent&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow";
-    }
     return (
-        <>
-            <Head>
-                <title>Create Next App</title>
-                <meta name="description" content="Custom Demonlist"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            </Head>
-
+        <Layout>
             <main>
-                <GoogleButton
-                    label='Sign in with Google'
-                    onClick={handleGoogleSignIn}
-                />
-                <DiscordButton />
             </main>
-        </>
+        </Layout>
     );
-}
+};
