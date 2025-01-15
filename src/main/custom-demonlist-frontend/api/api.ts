1738866263f5c556a2e88d4a8a4c1f9bac87ceb7
Dels1x <delsixreborn@gmail.com>
2025-01-15 23:55:57 +0200

    feat: added getCookie function for getting any cookie by name

--- src/main/custom-demonlist-frontend/api/api.ts
+++ src/main/custom-demonlist-frontend/api/api.ts
@@ -1,1 +1,18 @@
+import axios from "axios";
+
 const API_URL: string = "http://localhost:8080/";
+
+function getCookie(name: string): string | null {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return decodeURIComponent(match[2]);
+    return null;
+}
+
+const api = axios.create({
+        baseURL: API_URL,
+    });
+
+/* TODO
+api.interceptors.request.use(async (config) => {
+    let token = localStorage.getItem('access-token');
+})*/
