596e5ff7e405f3f704dc67e933ba05919512c312
delsix <delsixreborn@gmail.com>
2025-03-12 22:02:31 +0200

    fix: add constructors to DemonlistDoesntExistException

--- src/main/java/ua/delsix/exception/DemonlistDoesntExistException.java
+++ src/main/java/ua/delsix/exception/DemonlistDoesntExistException.java
@@ -1,5 +1,10 @@
 package ua.delsix.exception;
 
 public class DemonlistDoesntExistException extends Exception {
+    public DemonlistDoesntExistException(String message) {
+        super(message);
+    }
 
+    public DemonlistDoesntExistException() {
+    }
 }
