864049901958731bca275a7320fec8477b5301b7
delsix <delsixreborn@gmail.com>
2025-03-13 13:46:44 +0200

    feat: add updateAuthorById method in DemonRepository

--- src/main/java/ua/delsix/jpa/repository/DemonRepository.java
+++ src/main/java/ua/delsix/jpa/repository/DemonRepository.java
@@ -10,22 +10,28 @@ import ua.delsix.jpa.entity.Demon;
 @Repository
 public interface DemonRepository extends JpaRepository<Demon, Long> {
     int countByDemonlistId(long demonlistId);
+
     @Modifying
     @Transactional
     @Query("UPDATE Demon d SET d.placement = d.placement + 1 WHERE d.placement >= :placement AND d.demonlist.id = :demonlistId")
     void incrementPlacements(int placement, long demonlistId);
+
     @Modifying
     @Transactional
     @Query("UPDATE Demon d SET d.placement = d.placement + 1 WHERE d.placement >= :low AND d.placement < :high AND d.demonlist.id = :demonlistId")
     void incrementPlacementsBetween(int low, int high, long demonlistId);
+
     @Modifying
     @Transactional
     @Query("UPDATE Demon d SET d.placement = d.placement - 1 WHERE d.placement > :low AND d.placement <= :high AND d.demonlist.id = :demonlistId")
     void decrementPlacementsBetween(int low, int high, long demonlistId);
+
     boolean existsByPlacementAndDemonlistId(int placement, long demonlistId);
+
     @Modifying
     @Transactional
     void deleteByDemonlistPersonId(long personId);
+
     @Modifying
     @Transactional
     @Query("UPDATE Demon d SET d.placement = :placement WHERE d.id = :id")
@@ -33,6 +39,11 @@ public interface DemonRepository extends JpaRepository<Demon, Long> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE Demon d SET d.name = :newName WHERE d.id = :id")
-    void updateNameById(long id, String newName);
+    @Query("UPDATE Demon d SET d.name = :name WHERE d.id = :id")
+    void updateNameById(long id, String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Demon d SET d.author = :author WHERE d.id = :id")
+    void updateAuthorById(long id, String author);
 }
