abe27397a732a1c18ac936e86fe45294a7851784
Dels1x <delsixreborn@gmail.com>
2025-05-18 22:59:12 +0300

    feat: difficulty enum

---
+++ src/main/java/ua/delsix/enums/Difficulty.java
@@ -0,0 +1,15 @@
+package ua.delsix.enums;
+
+public enum Difficulty {
+    AUTO,
+    EASY,
+    NORMAL,
+    HARD,
+    HARDER,
+    INSANE,
+    EASY_DEMON,
+    MEDIUM_DEMON,
+    HARD_DEMON,
+    INSANE_DEMON,
+    EXTREME_DEMON
+}
